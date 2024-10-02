32c3ca3ddf499a8a71af49f49d8c1bdb4ffc0508
001084233 <liam.goldfinch@student.tafesa.edu.au>
2024-10-02 21:48:31 +0930

    Added GST feature

--- ProductMaintenance/ProductApps/MainWindow.xaml.cs
+++ ProductMaintenance/ProductApps/MainWindow.xaml.cs
@@ -36,6 +36,7 @@ namespace ProductApps
                 totalPaymentTextBlock.Text = Convert.ToString(cProduct.TotalPayment);
                 deliveryChargeTextBox.Text = Convert.ToString(cProduct.TotalPayment + 25.00m);
                 wrapChargeTextBox.Text = Convert.ToString(cProduct.TotalPayment + 25.00m + 5.00m);
+                gstTextBox.Text = Convert.ToString((cProduct.TotalPayment + 25.00m + 5.00m) * 1.1m);
             }
             catch (FormatException)
             {
